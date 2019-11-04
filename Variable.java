aa2b9b5f5b1011ed24c99e3ace6ce07292964e22
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-05 01:39:38 +0300

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable{
+	public static void main(String[] args){
+	byte a = 1;
+	short b = 12;
+	int c = 7700;
+	long d = 77004200;
+	float e = 4.2f;
+	double f = 4200.15d;
+	char g = 'I';
+	boolean comp = true;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	System.out.println(comp);
+	}
+}
