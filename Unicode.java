0782b65f08494c93b4d2f93b8272a0dbc261ff4c
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-05 15:11:20 +0300

    Update Unicode.java

--- Unicode.java
+++ Unicode.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 public class Unicode {
-    static Scanner s = new Scanner(System.in);
-
     public static void main(String[] args) {
-          for (char i = '\u0021'; i < '\u007e'; i++) {
-          System.out.println(i);
+        for (char i = '\u0021'; i < '\u007e'; i++) {
+            System.out.println(i);
         }
     }
 }
