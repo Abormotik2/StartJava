0ddd788014e7f5b6e442179e6ef06656e3ab19a4
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-05 15:08:43 +0300

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
-	public static void main(String[] args) {
-	 byte processor = 1;
-	 short kernels  = 8;
-	 int model = 7700;
-	 long idproduct = 33880088;
-	 float ram = 32.0f;
-	 double ramspeed = 3600.00d;
-	 char processorid = 'I';
-	 boolean comp = true; 
+    public static void main(String[] args) {
+        byte processor = 1;
+        short kernels = 8;
+        int model = 7700;
+        long idproduct = 33880088;
+        float ram = 32.0f;
+        double ramspeed = 3600.00d;
+        char processorid = 'I';
+        boolean comp = true;
 
-	 System.out.println(processor);
-	 System.out.println(kernels);
-	 System.out.println(model);
-	 System.out.println(idproduct);
-	 System.out.println(ram);
-	 System.out.println(ramspeed);
-	 System.out.println(processorid);
-	 System.out.println(comp);
-	}
+        System.out.println(processor);
+        System.out.println(kernels);
+        System.out.println(model);
+        System.out.println(idproduct);
+        System.out.println(ram);
+        System.out.println(ramspeed);
+        System.out.println(processorid);
+        System.out.println(comp);
+    }
 }
