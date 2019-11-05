8469a51a39331e9a092ff123f7f0e0015e020eb5
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-05 15:35:24 +0300

    Update Cycle.java

--- Cycle.java
+++ Cycle.java
@@ -1,28 +1,24 @@
-public class Cycle{
+public class Cycle {
     public static void main(String[] args) {
         for (int i = 0; i <= 20; i++) {
             System.out.println(i);
-
         }
         System.out.println(" ");
-        int b = 6;
-        while (b >= -6) {
-            System.out.println(b);
-            b = b - 2;
-
+        int counter = 6;
+        while (counter >= -6) {
+            System.out.println(counter);
+            counter -= 2;
         }
         System.out.println(" ");
 
         int x = 10;
-        int y = 0;
+        int sumOdd = 0;
         do {
             if (x % 2 != 0) {
-                y += x;
+                sumOdd += x;
             }
-
         }
         while (x++ < 20);
-        System.out.println(y);
-
+        System.out.println(sumOdd);
     }
 }
