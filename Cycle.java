8655e58b2c7cc6652a73b6bf7d0268d9e5d60dd8
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-06 22:26:34 +0300

    Update Cycle.java

--- Cycle.java
+++ Cycle.java
@@ -3,14 +3,15 @@ public class Cycle {
         for (int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
+
         System.out.println(" ");
         int counter = 6;
         while (counter >= -6) {
             System.out.println(counter);
             counter -= 2;
         }
+        
         System.out.println(" ");
-
         int x = 10;
         int sumOdd = 0;
         do {
