de037b0552f5ab0326571c0d13bdd1a916512e51
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-06 21:39:23 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,70 +1,27 @@
-import java.util.Scanner;
 public class Calculator {
-        static Scanner s = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int num1 = num();
-        int num2 = num();
-        char move = operation();
-        int res = calc(num1, num2, move);
-        System.out.println(res);
-    }
-
-    public static int num() {
-        System.out.println("Edit number");
-        int num4;
-        if (s.hasNextInt() && (num4 = s.nextInt()) > 0) {
-            return num4;
+        int num1 = 8;
+        int num2 = 3;
+        char symbol = '^';
+        
+        if (symbol == '+') {
+            System.out.println(num1 + num2);
+        } else if (symbol == '-') {
+            System.out.println(num1 - num2);
+        } else if (symbol == '*') {
+            System.out.println(num1 * num2);
+        } else if (symbol == '/') {
+            System.out.println(num1 / num2);
+        } else if (symbol == '%') {
+            System.out.println(num1 % num2);
+        } else if (symbol == '^') {
+            int result = 1;
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+            System.out.println(result);
         } else {
-            System.out.println("It's not positive number, or not number yet!");
-            s.next();
-            return num();
+            System.out.println("Operation not found");
         }
-    }
-
-    public static char operation() {
-        System.out.println("Edit operation");
-        char move;
-        if (s.hasNext()) {
-            move = s.next().charAt(0);
-            return move;
-        } else {
-            System.out.println("Not now");
-            s.next();
-            return operation();
-        }
-    }
-
-
-
-    public static int calc(int num1, int num2, char move) {
-        int res;
-        switch (move) {
-            case '+':
-                res = num1 + num2;
-                break;
-
-            case '-':
-                res = num1 - num2;
-                break;
-
-            case '*':
-                res = num1 * num2;
-                break;
-
-            case '/':
-                res = num1 / num2;
-                break;
-            case '^':
-                res = (int) Math.pow(num1, num2);
-                break;
-            case '%':
-                res = num1 % num2;
-                break;
-            default:
-                System.out.println("Operation not found");
-                res = calc(num1, num2, operation());
-        }
-        return res;
     }
 }
