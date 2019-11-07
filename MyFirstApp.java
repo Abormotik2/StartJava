7bf73d78fecaaf91c43de764bf7d09ad77d19023
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-07 20:18:04 +0300

    Delete MyFirstApp.java

--- MyFirstApp.java
+++
@@ -1,8 +0,0 @@
-public class MyFirstApp {	
-    public static void main(String[] args) {
-    	int a = 5;
-    	int b = 10;
-    	System.out.println(a + b);
-        System.out.println("Hello, world");
-    }
-}
