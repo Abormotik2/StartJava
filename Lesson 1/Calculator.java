862f43f945cab3de537a8537bd28ef240a9a6fa1
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-07 20:18:15 +0300

    Create Calculator.java

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 3;
+        char symbol = '^';
+        
+        if (symbol == '+') {
+            System.out.println(num1 + num2);
+        } else if (symbol == '-') {
+            System.out.println(num1 - num2);
+        } else if (symbol == '*') {
+            System.out.println(num1 * num2);
+        } else if (symbol == '/') {
+            System.out.println(num1 / num2);
+        } else if (symbol == '%') {
+            System.out.println(num1 % num2);
+        } else if (symbol == '^') {
+            int result = 1;
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+            System.out.println(result);
+        } else {
+            System.out.println("Operation not found");
+        }
+    }
+}
