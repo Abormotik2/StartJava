ac6645f210b727f189fbaf34f2aaa731d8febdd9
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-07 20:18:21 +0300

    Create Cycle.java

---
+++ Lesson 1/Cycle.java
@@ -0,0 +1,25 @@
+public class Cycle {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println(" ");
+        int counter = 6;
+        while (counter >= -6) {
+            System.out.println(counter);
+            counter -= 2;
+        }
+        
+        System.out.println(" ");
+        int x = 10;
+        int sumOdd = 0;
+        do {
+            if (x % 2 != 0) {
+                sumOdd += x;
+            }
+        }
+        while (x++ < 20);
+        System.out.println(sumOdd);
+    }
+}
