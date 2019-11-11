8c0927698df047b59016deb444a46fbd9a6e41f6
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-11 16:59:52 +0300

    Create Wolf.java

---
+++ Lesson 2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    boolean isMan;
+    String name;
+    float weight;
+    byte age;
+    char color;
+
+    boolean walk() {
+        return true;
+    }
+
+    boolean sit() {
+        return true;
+    }
+
+    boolean run() {
+        return true;
+    }
+
+    void howl() {
+
+    }
+
+    boolean hunt() {
+        return true;
+    }
+}
