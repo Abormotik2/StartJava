6b19032b414f5c7d201126ffd1877146a52b6e1d
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-11 19:54:38 +0300

    Update Wolf.java

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,27 +1,72 @@
 public class Wolf {
-    boolean isMan;
-    String name;
-    float weight;
-    byte age;
-    char color;
+    private boolean isMan;
 
-    boolean walk() {
+    public boolean isMan() {
+        return isMan;
+    }
+
+    public void setMan(boolean man) {
+        isMan = man;
+    }
+
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private float weight;
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    private int age;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Incorrect age!");
+        } else {
+            this.age = age;
+        }
+    }
+
+    private char color;
+    public char getColor() {
+        return color;
+    }
+
+    public void setColor(char color) {
+        this.color = color;
+    }
+
+
+    public boolean walk() {
         return true;
     }
 
-    boolean sit() {
+    public boolean sit() {
         return true;
     }
 
-    boolean run() {
+    public boolean run() {
         return true;
     }
 
-    void howl() {
+    public void howl() {
 
     }
 
-    boolean hunt() {
+    public boolean hunt() {
         return true;
     }
 }
