834146641cc16007c4effef76fa93db1ee7a8885
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-11 17:42:37 +0300

    Update WolfTest.java

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -3,15 +3,16 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
         wolf.isMan = true;
-        wolf.name = "Scar";
+        System.out.println(wolf.isMan);
+        wolf.name = "Шрам";
+        System.out.println(wolf.name);
         wolf.weight = 30.5f;
+        System.out.println(wolf.weight);
         wolf.age = 10;
+        System.out.println(wolf.age);
         wolf.color = 'w';//white
-        System.out.println("Wolf isMan sex: " + wolf.isMan);
-        System.out.println("Wolf name is: " + wolf.name);
-        System.out.println("Wolw weight: " + wolf.weight);
-        System.out.println("Wolf age: " + wolf.age);
-        System.out.println("Wolf color: " + wolf.color);
+        System.out.println(wolf.color);
+        
         wolf.walk();
         wolf.sit();
         wolf.run();
