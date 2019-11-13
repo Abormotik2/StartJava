45afc42f72d9e2f2f14196fc57ccffa307403f73
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-13 15:00:18 +0300

    Update CalculatorTest.java

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -5,23 +5,25 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
+        boolean flag = true;
         do {
             calc.setNum1(calc.numFirst());
             calc.setMove(calc.operation());
             calc.setNum2(calc.numSecond());
             calc.setRes(calc.calc(calc.getNum1(), calc.getMove(), calc.getNum2()));
             System.out.println("Result: " + calc.getRes());
-            while (!scan.equals("yes") || !scan.equals("no")) {
+            while (flag) {
                 System.out.println("Do u,r want to continue? Say yes or no...");
-                if (scan.equals("no")) {
+                String answer = scan.nextLine();
+                if (answer.equals("no")) {
                     System.out.println("Bye!");
-                    break;
-                } else if (scan.nextLine().equals("yes")) {
+                    flag = false;
+                } else if (answer.equals("yes")) {
                     System.out.println("Go!");
                     break;
                 }
             }
         }
-        while (scan.equals("yes"));
+        while (flag);
     }
 }
