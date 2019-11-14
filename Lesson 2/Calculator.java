8ffa6d1b7dfb943aad130744d38131088a5b3ad5
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-14 21:06:30 +0300

    Update Calculator.java

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
-
 public class Calculator {
-    static Scanner s = new Scanner(System.in);
-    private int num1;
-    private char move;
-    private int res;
-    private int num2;
+    private int num1 = -1;
+    private int num2 = -1;
+    private char move = '\0';
 
     public int getNum1() {
 
         return num1;
     }
 
-    public void setNum1(int num1) {
-
-        this.num1 = num1;
+    public int setNum1(int num1) {
+        if (num1 >= 0) {
+            this.num1 = num1;
+        } else {
+            System.out.println("It's not positive number, or not number yet!");
+        }
+        return getNum1();
     }
 
     public char getMove() {
@@ -22,93 +22,50 @@ public class Calculator {
         return move;
     }
 
-    public void setMove(char move) {
-
-        this.move = move;
+    public char setMove(char move) {
+        switch (move) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+            case '%':
+                this.move = move;
+                break;
+            default:
+                System.out.println("Don't support type!");
+        }
+        return getMove();
     }
 
     public int getNum2() {
         return num2;
     }
 
-    public void setNum2(int num2) {
-
-        this.num2 = num2;
+    public int setNum2(int num2) {
+        if (num2 >= 0) {
+            this.num2 = num2;
+        } else {
+            System.out.println("It's not positive number, or not number yet!");
+        }
+        return getNum2();
     }
 
     public int getRes() {
-
-        return res;
-    }
-
-    public void setRes(int res) {
-
-        this.res = res;
-    }
-
-    public int numFirst() {
-        System.out.println("Edit first number : ");
-        int num4;
-        if (s.hasNextInt() && (num4 = s.nextInt()) > 0) {
-            return num4;
-        } else {
-            System.out.println("It's not positive number, or not number yet!");
-            return numFirst();
-        }
-    }
-
-    public char operation() {
-        System.out.println("Edit the operation: ");
-        char op;
-        if (s.hasNext()) {
-            op = s.next().charAt(0);
-            return op;
-        } else {
-            System.out.println("Try again!");
-            return operation();
-        }
-    }
-
-    public int calc(int num1, char move, int num2) {
-        int res;
         switch (move) {
             case '+':
-                res = num1 + num2;
-                break;
-
+                return num1 + num2;
             case '-':
-                res = num1 - num2;
-                break;
-
+                return num1 - num2;
             case '*':
-                res = num1 * num2;
-                break;
-
+                return num1 * num2;
             case '/':
-                res = num1 / num2;
-                break;
+                return num1 / num2;
             case '^':
-                res = (int) Math.pow(num1, num2);
-                break;
+                return (int) Math.pow(num1, num2);
             case '%':
-                res = num1 % num2;
-                break;
-            default:
-                System.out.println("Operation not found");
-                res = calc(num1, operation(), num2);
+                return num1 % num2;
         }
-        return res;
-    }
-
-    public int numSecond() {
-        System.out.println("Edit second number: ");
-        int num5;
-        if (s.hasNextInt() && (num5 = s.nextInt()) > 0) {
-            return num5;
-        } else {
-            System.out.println("It's not positive number, or not number yet!");
-            s.next();
-            return numSecond();
-        }
+        throw new IllegalArgumentException("Calc Error!");
     }
 }
