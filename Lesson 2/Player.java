90bd949796756abbd342d4569447264a7e6f1595
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-15 22:01:49 +0300

    Create Player.java

---
+++ Lesson 2/Player.java
@@ -0,0 +1,19 @@
+public class Player {
+    private String name;
+    private int number = 0;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
