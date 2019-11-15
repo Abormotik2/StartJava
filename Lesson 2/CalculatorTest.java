ff7bdc2dc39014566cc41c614a859f063e814d74
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-15 15:57:39 +0300

    Update CalculatorTest.java

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calc;
+        boolean isFlag = true;
         do {
             calc = new Calculator();
             System.out.println("Edit first number : ");
@@ -13,8 +15,16 @@ public class CalculatorTest {
             System.out.println("Edit second number : ");
             while (calc.setNum2(scan.nextInt()) < 0);
             System.out.println("Result: " + calc.getRes());
-            System.out.println("Do u,r want to continue? Say yes or no...");
-        }
-        while (scan.nextLine().equals("yes"));
+            while (isFlag) {
+                System.out.println("Do u,r want to continue? Say yes or no...");
+                String answer = scan.nextLine();
+                if (answer.equals("no")) {
+                    System.out.println("Bye");
+                    isFlag = false;
+                } else if (answer.equals("yes")) {
+                    break;
+                }
+            }
+        } while (isFlag);
     }
 }
