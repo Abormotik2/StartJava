3d3035e7a29286d1c1ffde1a32a1fc2c4d3cf60f
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-18 18:51:00 +0300

    Update GuessNumber.java

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -1,58 +1,33 @@
 import java.util.Scanner;
 
 public class GuessNumber {
-    private Scanner scan = new Scanner(System.in);
-    private int pcNum = (int) (Math.random() * 100);
-    private boolean p1Right = false;
-    private boolean p2Right = false;
-    private String answer;
+    private int pcNum;
+    private Player p1;
+    private Player p2;
+    private Scanner scanner;
 
+    public GuessNumber(Player player1, Player player2, Scanner scanner) {
+        this.p1 = player1;
+        this.p2 = player2;
+        this.scanner = scanner;
+    }
 
     public void startGame() {
-        System.out.println("Call the first player: ");
-        Player p1 = new Player(scan.nextLine());
-        System.out.println("Call the second player: ");
-        Player p2 = new Player(scan.nextLine());
+        pcNum = (int) (Math.random() * 101);
         System.out.println("And now, lets start " + p1 + " and " + p2);
+        Player currentPlayer = p1;
         do {
-            System.out.println(p1 + " input ur number pls: ");
-            p1.setNumber(scan.nextInt());
-            System.out.println(p2 + " input ur number pls: ");
-            p2.setNumber(scan.nextInt());
-            if (p1.getNumber() == pcNum) {
-                p1Right = true;
-            } else if (p1.getNumber() > pcNum) {
-                System.out.println(p1 + "'s" + " num is bigger then mine");
+            System.out.println(currentPlayer.getName() + " play game ");
+            int count = currentPlayer.askNumber();
+            if (count == pcNum) {
+                System.out.println("Great, " + currentPlayer.getName() + " win!");
+                break;
+            } else if (count > pcNum) {
+                System.out.println(currentPlayer.getName() + "'s" + " num is bigger then mine");
             } else {
-                System.out.println(p1 + "'s" + " num is smaller then mine");
+                System.out.println(currentPlayer.getName() + "'s" + " num is smaller then mine");
             }
-            if (p2.getNumber() == pcNum) {
-                p2Right = true;
-            } else if (p2.getNumber() > pcNum) {
-                System.out.println(p2 + "'s" + " num is bigger then mine");
-            } else {
-                System.out.println(p2 + "'s" + " num is smaller then mine");
-            }
-            if (p1Right || p2Right) {
-                System.out.println("Great we have a champion!");
-                System.out.println(p1 + " guess? " + p1Right);
-                System.out.println(p2 + " guess? " + p2Right);
-                System.out.println("This game is end!");
-                p1Right = false;
-                p2Right = false;
-                while (true) {
-                    System.out.println("Do u want to continue? Say yes or no...");
-                    answer = scan.nextLine();
-                    if (answer.equals("no")) {
-                        System.out.println("Bye");
-                        break;
-                    } else if (answer.equals("yes")) {
-                        break;
-                    }
-                }
-            } else {
-                System.out.println("Try again guys!");
-            }
+            currentPlayer = currentPlayer == p1 ? p2 : p1;
         } while (true);
     }
 }
