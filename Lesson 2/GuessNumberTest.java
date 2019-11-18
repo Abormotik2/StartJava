eb17702cd43d3216a6ae8d716ab1774bc751c3b0
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-18 18:51:04 +0300

    Update GuessNumberTest.java

--- Lesson 2/GuessNumberTest.java
+++ Lesson 2/GuessNumberTest.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class GuessNumberTest {
     public static void main(String[] args) {
-        GuessNumber game = new GuessNumber();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Edit first name: ");
+        Player p1 = new Player(scan.nextLine(),scan);
+        System.out.println("Edit second name: ");
+        Player p2 = new Player(scan.nextLine(),scan);
+        GuessNumber game = new GuessNumber(p1,p2,scan);
         game.startGame();
+        String answer;
+        while (true) {
+            System.out.println("Do u want to continue? Say yes or no...");
+            answer = scan.nextLine();
+            if (answer.equals("no")) {
+                System.out.println("Bye");
+                break;
+            } else if (answer.equals("yes")) {
+                game.startGame();
+            }
+        }
     }
 }
