be495e006f68a3c9aeb58c99156c518c530ad75b
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-20 11:47:33 +0300

    Update GuessNumber.java

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -17,12 +17,12 @@ public class GuessNumber {
         Player currentPlayer = p1;
         do {
             System.out.println(currentPlayer.getName() + " play game ");
-            int count = scanner.nextInt();
-            currentPlayer.setNumber(count);
-            if (count == pcNum) {
+            currentPlayer.setNumber(scanner.nextInt());
+            int number = currentPlayer.getNumber();
+            if (number == pcNum) {
                 System.out.println("Great, " + currentPlayer.getName() + " win!");
                 break;
-            } else if (count > pcNum) {
+            } else if (number > pcNum) {
                 System.out.println(currentPlayer.getName() + "'s" + " num is bigger then mine");
             } else {
                 System.out.println(currentPlayer.getName() + "'s" + " num is smaller then mine");
