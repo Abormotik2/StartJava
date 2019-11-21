f47daa0c9c7cb4ceeaab665b5c63e7566379d543
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-21 23:08:24 +0300

    Create Unicode.java

---
+++ src/com/startJava/lesson_1/unicode/Unicode.java
@@ -0,0 +1,9 @@
+package com.startjava.lesson_1.unicode;
+
+public class Unicode {
+    public static void main(String[] args) {
+        for (char i = '\u0021'; i < '\u007e'; i++) {
+            System.out.println(i);
+        }
+    }
+}
