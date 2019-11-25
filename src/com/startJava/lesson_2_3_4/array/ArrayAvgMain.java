187511de9ca84b347b55b959969061a208d2756f
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-25 16:18:31 +0300

    Create ArrayAvgMain.java

---
+++ src/com/startJava/lesson_2_3_4/array/ArrayAvgMain.java
@@ -0,0 +1,26 @@
+package com.startJava.lesson_2_3_4.array;
+
+import java.util.Scanner;
+
+public class ArrayAvgMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input the number .... of array: ");
+        int maxl = scanner.nextInt();
+        int[] numbers = new int[maxl];
+        System.out.print("Input the number into array: ");
+        int sum = 0;
+        int avg = 0;
+        for (int i = 0; i < maxl; i++) {
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+            avg = sum / maxl;
+        }
+        for (int i = 0; i < maxl; i++) {
+            if (numbers[i] < avg) {
+                System.out.println("The low compare(avg) number is: " + numbers[i]);
+            }
+
+        }
+    }
+}
