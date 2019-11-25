09dadbd0066c4b4fc6d3d2b0f688ee3252fd3aca
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-25 16:18:46 +0300

    Create Calculator.java

---
+++ src/com/startJava/lesson_2_3_4/calculator/Calculator.java
@@ -0,0 +1,59 @@
+package com.startJava.lesson_2_3_4.calculator;
+
+public class Calculator {
+    private int num1 = -1;
+    private int num2 = -1;
+    private char sign = '\0';
+
+    public int setNum1(int num1) {
+        if (num1 >= 0) {
+            this.num1 = num1;
+        } else {
+            System.out.println("It's not positive number, or not number yet!");
+        }
+        return num1;
+    }
+
+    public char setMove(char sign) {
+        switch (sign) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+            case '%':
+                this.sign = sign;
+                break;
+            default:
+                System.out.println("Don't support type!");
+        }
+        return sign;
+    }
+
+    public int setNum2(int num2) {
+        if (num2 >= 0) {
+            this.num2 = num2;
+        } else {
+            System.out.println("It's not positive number, or not number yet!");
+        }
+        return num2;
+    }
+
+    public int calculate() {
+        switch (sign) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            case '^':
+                return (int) Math.pow(num1, num2);
+            case '%':
+                return num1 % num2;
+        }
+        throw new IllegalArgumentException("Calc Error!");
+    }
+}
