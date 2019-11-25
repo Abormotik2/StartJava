4c9c268b36ad681d63b43177dc560aabea56e508
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-25 16:19:03 +0300

    Create Jaeger.java

---
+++ src/com/startJava/lesson_2_3_4/robot/Jaeger.java
@@ -0,0 +1,57 @@
+package com.startJava.lesson_2_3_4.robot;
+
+public class Jaeger {
+
+    private String modelName = "";
+    private String mark = "";
+    private String origin = "";
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public Jaeger(String modelName, String mark, String origin) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+    }
+
+    public Jaeger(String modelName, String mark, String origin, float height, float weight) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Jaeger(String modelName, String mark, String origin, int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public boolean drift() {
+        System.out.println("Ur using drift!");
+        return true;
+    }
+
+    public void move() {
+        System.out.println("U has walked five steps!");
+    }
+
+    public String scanKaiju() {
+        return "nothing";
+    }
+
+    public void useVortexCannon() {
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ModelName = %s\n mark = %s\n origin = %s\n weight = %f\n height = %f\n speed = %d\n strength = %d\n armor = %d\n", modelName, mark, origin, weight, height,speed,strength,armor);
+    }
+}
