d32b6d5a245fd78b43c6f80b76f2b0f1c2cfcbb1
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-25 16:18:58 +0300

    Create Person.java

---
+++ src/com/startJava/lesson_2_3_4/person/Person.java
@@ -0,0 +1,29 @@
+package com.startJava.lesson_2_3_4.person;
+
+public class Person {
+    String name = "Vasya";
+    boolean isMan = true;
+    float height = 192.5f;
+    double weight = 88.5d;
+    int age = 28;
+
+    void walk() {
+
+    }
+
+    void run() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void learJava() {
+
+    }
+
+    void speak() {
+
+    }
+}
