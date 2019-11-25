8f076037b2e0dea53d3cae9ec009431b69127ddc
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-25 16:19:00 +0300

    Create JaegerTest.java

---
+++ src/com/startJava/lesson_2_3_4/robot/JaegerTest.java
@@ -0,0 +1,17 @@
+package com.startJava.lesson_2_3_4.robot;
+
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        Jaeger jaegerOne = new Jaeger("Gipsy Danger", "Mark-3", "USA");
+        System.out.println(jaegerOne);
+        Jaeger jaegerTwo = new Jaeger("Striker Eureka", "Mark-5", "USA", 76.2f, 1.850f);
+        System.out.println(jaegerTwo);
+        Jaeger jaegerThree = new Jaeger("Cayote Tango", "Mark-1", "USA", 5 ,7,4);
+        System.out.println(jaegerThree);
+        jaegerOne.drift();
+        jaegerTwo.move();
+        jaegerTwo.useVortexCannon();
+        jaegerThree.scanKaiju();
+    }
+}
