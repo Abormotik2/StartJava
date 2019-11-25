840f0ffbdced570d46f16856f6db81cfadbb4dcd
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-25 16:18:56 +0300

    Create Player.java

---
+++ src/com/startJava/lesson_2_3_4/game/Player.java
@@ -0,0 +1,27 @@
+package com.startJava.lesson_2_3_4.game;
+
+public class Player {
+    private String name;
+    private int number;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
