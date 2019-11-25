79ac26e00fb8e2afd5d9d2a844e676569e646d68
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-25 17:07:24 +0300

    Update Calculator.java

--- src/com/startJava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startJava/lesson_2_3_4/calculator/Calculator.java
@@ -1,13 +1,13 @@
 package com.startJava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    private int num1 = -1;
-    private int num2 = -1;
-    private char sign = '\0';
+    private int[] num1 = new int[1];
+    private int[] num2 = new int[1];
+    private char[] sign = new char[1];
 
     public int setNum1(int num1) {
         if (num1 >= 0) {
-            this.num1 = num1;
+            this.num1[0] = num1;
         } else {
             System.out.println("It's not positive number, or not number yet!");
         }
@@ -22,7 +22,7 @@ public class Calculator {
             case '/':
             case '^':
             case '%':
-                this.sign = sign;
+                this.sign[0] = sign;
                 break;
             default:
                 System.out.println("Don't support type!");
@@ -32,7 +32,7 @@ public class Calculator {
 
     public int setNum2(int num2) {
         if (num2 >= 0) {
-            this.num2 = num2;
+            this.num2[0] = num2;
         } else {
             System.out.println("It's not positive number, or not number yet!");
         }
@@ -40,19 +40,19 @@ public class Calculator {
     }
 
     public int calculate() {
-        switch (sign) {
+        switch (sign[0]) {
             case '+':
-                return num1 + num2;
+                return num1[0] + num2[0];
             case '-':
-                return num1 - num2;
+                return num1[0] - num2[0];
             case '*':
-                return num1 * num2;
+                return num1[0] * num2[0];
             case '/':
-                return num1 / num2;
+                return Math.floorDiv(num1[0], num2[0]);
             case '^':
-                return (int) Math.pow(num1, num2);
+                return (int) Math.pow(num1[0], num2[0]);
             case '%':
-                return num1 % num2;
+                return num1[0] % num2[0];
         }
         throw new IllegalArgumentException("Calc Error!");
     }
