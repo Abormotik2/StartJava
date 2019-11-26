f9bd382d80b3f94bf328e532ff26cca435f8e693
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-26 18:07:04 +0300

    Update CalculatorTest.java

--- src/com/startJava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startJava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,21 +1,19 @@
 package com.startJava.lesson_2_3_4.calculator;
-
 import java.util.Scanner;
 
 public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Calculator calc = new Calculator();
+        Calculator calc;
+        String expression;
         String answer;
         System.out.println("Calculator start!");
         do {
             System.out.print("Input the num1/operation/num2 into array by space: ");
-            String expression = scan.nextLine();
-            String[] symbols = expression.split(" ");
-            calc.setNum1(Integer.parseInt(symbols[0]));
-            calc.setOperation(symbols[1].charAt(0));
-            calc.setNum2(Integer.parseInt(symbols[2]));
+            expression = scan.nextLine();
+            calc = new Calculator(expression.split(" "));
+            calc.operation();
             System.out.println("Result: " + calc.calculate());
             do {
                 System.out.print("Do u want to continue? Say yes or no...");
