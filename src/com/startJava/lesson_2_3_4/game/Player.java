8e6b44eea25d7aa7e0883a338423b030e57d2e1b
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-27 23:34:21 +0300

    Update Player.java

--- src/com/startJava/lesson_2_3_4/game/Player.java
+++ src/com/startJava/lesson_2_3_4/game/Player.java
@@ -3,6 +3,7 @@ package com.startJava.lesson_2_3_4.game;
 public class Player {
     private String name;
     private int number;
+    private int[] arrayNum;
 
     public Player(String name) {
         this.name = name;
@@ -20,6 +21,13 @@ public class Player {
         return number;
     }
 
+    public int[] getArrayNum() {
+        return arrayNum;
+    }
+
+    public void setArrayNum(int[] arrayNum) {
+        this.arrayNum = arrayNum;
+    }
     @Override
     public String toString() {
         return name;
