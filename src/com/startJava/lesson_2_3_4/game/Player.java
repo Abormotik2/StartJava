1c5de2614ceb0863b2a9a838308653e232a60375
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-28 22:46:23 +0300

    Update Player.java

--- src/com/startJava/lesson_2_3_4/game/Player.java
+++ src/com/startJava/lesson_2_3_4/game/Player.java
@@ -1,11 +1,32 @@
 package com.startJava.lesson_2_3_4.game;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
-    private int[] attempts;
+    private int[] attempts = new int[10];
+    private int iterator = 0;
 
     public Player(String name) {
         this.name = name;
+    }
+
+    public void addAttempts(int number) {
+        attempts[iterator] = number;
+        iterator++;
+    }
+
+    public int lastNum() {
+        return iterator;
+    }
+
+    public boolean ifLose() {
+        if (iterator > 10) {
+            System.out.println("Players, you have run out of attempts ");
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public String getName() {
@@ -13,14 +34,12 @@ public class Player {
     }
 
     public int[] getAttempts() {
-        return attempts;
+        return Arrays.copyOf(attempts, iterator);
     }
 
-    public void setAttempts(int[] arrayNum) {
-        this.attempts = arrayNum;
-    }
     @Override
     public String toString() {
         return name;
     }
+
 }
