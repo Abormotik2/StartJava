b23169ed26ba137171ac4d6c29338da74a602a81
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-11-29 17:47:48 +0300

    Update GuessNumber.java

--- src/com/startJava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startJava/lesson_2_3_4/game/GuessNumber.java
@@ -7,33 +7,55 @@ public class GuessNumber {
     private Player p1;
     private Player p2;
     private Scanner scanner = new Scanner(System.in);
+    private int pcNum;
+    private int number;
+    private Player currentPlayer;
 
     public GuessNumber(Player player1, Player player2) {
         this.p1 = player1;
         this.p2 = player2;
     }
 
+    public void addNum() {
+        number = scanner.nextInt();
+    }
+
     public void startGame() {
-        int pcNum = (int) (Math.random() * 101);
+        pcNum = (int) (Math.random() * 101);
         System.out.println("You have 10 attempts! " + p1 + " and " + p2);
-        Player currentPlayer = p1;
-        do {
-            currentPlayer.ifLose();
-            System.out.println(currentPlayer.getName() + " play game ");
-            int number = scanner.nextInt();
-            currentPlayer.addAttempts(number);
-            if (number == pcNum) {
-            System.out.println("Great, " + currentPlayer.getName() + " guessNumber = " + pcNum + " with " + currentPlayer.lastNum() + " attempts");
+        currentPlayer = p1;
+        while (true){
+            if (currentPlayer.lastNum() > 9) {
+                System.out.println("Players, you have run out of attempts ");
                 System.out.println(p1.getName() + "s numbers is " + Arrays.toString(p1.getAttempts()));
                 System.out.println(p2.getName() + "s numbers is " + Arrays.toString(p2.getAttempts()));
                 break;
-            } else if (number > pcNum) {
-                System.out.println(currentPlayer.getName() + "'s" + " num is bigger then mine");
-            } else {
-                System.out.println(currentPlayer.getName() + "'s" + " num is smaller then mine");
+            }
+            System.out.println(currentPlayer.getName() + " play game ");
+            addNum();
+            currentPlayer.addAttempt(number);
+            if(!checkNum(number)){
+                break;
             }
             currentPlayer = currentPlayer == p1 ? p2 : p1;
+        }
+    }
 
-        } while (true);
+    private boolean checkNum(int number) {
+        if (number == pcNum) {
+            System.out.println("Great, " + currentPlayer.getName() + " guessNumber = " + pcNum + " with " + currentPlayer.lastNum() + " attempts");
+            System.out.println(p1.getName() + "s numbers is " + Arrays.toString(p1.getAttempts()));
+            System.out.println(p2.getName() + "s numbers is " + Arrays.toString(p2.getAttempts()));
+            return false;
+        } else if (number > pcNum) {
+            System.out.println(currentPlayer.getName() + "'s" + " num is bigger then mine");
+            return true;
+        } else {
+            System.out.println(currentPlayer.getName() + "'s" + " num is smaller then mine");
+            return true;
+        }
     }
 }
+
+
+
