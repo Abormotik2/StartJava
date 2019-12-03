b6bc04084afca78b466114cbe0ad62ce8b9eb601
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-03 22:59:53 +0300

    Update querise.sql

--- src/com/startJava/lesson_5/querise.sql
+++ src/com/startJava/lesson_5/querise.sql
@@ -4,14 +4,14 @@ SELECT * FROM robots;
 
 SELECT * FROM robots WHERE status = 'Active';
 
-SELECT * FROM robots WHERE mark = 'Mark-1' OR mark = 'Mark-7';
+SELECT * FROM robots WHERE mark IN('Mark-1','Mark-7');
 
 SELECT * FROM robots ORDER BY mark;
 
-SELECT * FROM robots WHERE launch IN(SELECT MAX(launch) From ROBOTS);
+SELECT * FROM robots WHERE launch IN(SELECT MAX(launch) FROM ROBOTS);
 
-SELECT * FROM robots WHERE kaijukill IN(SELECT MAX(kaijukill) From robots);
-SELECT * FROM robots WHERE kaijukill IN(SELECT MIN(kaijukill) From robots);
+SELECT * FROM robots WHERE kaijukill = (SELECT MAX(kaijukill) FROM robots);
+SELECT * FROM robots WHERE kaijukill = (SELECT MIN(kaijukill) FROM robots);
 SELECT AVG(weight) as avgWeight FROM Robots;
 
 UPDATE robots SET kaijukill = kaijukill + 1 WHERE status = 'Active';
