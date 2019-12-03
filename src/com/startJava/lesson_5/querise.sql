fe48d7e30cb958f37442b84f1d67f6db636a0b2f
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-03 22:32:02 +0300

    Update querise.sql

--- src/com/startJava/lesson_5/querise.sql
+++ src/com/startJava/lesson_5/querise.sql
@@ -1,19 +1,19 @@
 -- querise
 
-SELECT * FROM Robots;
+SELECT * FROM robots;
 
-SELECT * FROM Robots WHERE status = 'Active';
+SELECT * FROM robots WHERE status = 'Active';
 
-SELECT * FROM Robots WHERE mark = 'Mark-1' OR mark = 'Mark-7';
+SELECT * FROM robots WHERE mark = 'Mark-1' OR mark = 'Mark-7';
 
-SELECT * FROM Robots ORDER BY mark;
+SELECT * FROM robots ORDER BY mark;
 
-SELECT * FROM Robots WHERE launch IN(SELECT MAX(launch) From ROBOTS);
+SELECT * FROM robots WHERE launch IN(SELECT MAX(launch) From ROBOTS);
 
-SELECT * FROM Robots WHERE kaijukill IN(SELECT MAX(kaijukill) From ROBOTS);
-SELECT * FROM Robots WHERE kaijukill IN(SELECT MIN(kaijukill) From ROBOTS);
+SELECT * FROM robots WHERE kaijukill IN(SELECT MAX(kaijukill) From robots);
+SELECT * FROM robots WHERE kaijukill IN(SELECT MIN(kaijukill) From robots);
 SELECT AVG(weight) as avgWeight FROM Robots;
 
-UPDATE ROBOTS SET kaijukill = kaijukill + 1 WHERE status = 'Active';
+UPDATE robots SET kaijukill = kaijukill + 1 WHERE status = 'Active';
 
-DELETE FROM ROBOTS WHERE status = 'Destroyed';
+DELETE FROM robots WHERE status = 'Destroyed';
