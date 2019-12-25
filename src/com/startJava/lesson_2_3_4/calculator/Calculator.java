077c193ce026b522c9688d0a6c859d0a97cd3c0e
Abormotik2 <57225233+Abormotik2@users.noreply.github.com>
2019-12-25 20:29:28 +0300

    made variables like BigDecimal. Delete checks Add try  - catch, to catch wrong orthography exception Change the operations in Switch case check

--- src/com/startJava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startJava/lesson_2_3_4/calculator/Calculator.java
@@ -1,9 +1,13 @@
 package com.startJava.lesson_2_3_4.calculator;
 
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.ROUND_HALF_UP;
+
 public class Calculator {
 
-    private double num1;
-    private double num2;
+    private BigDecimal num1 = new BigDecimal(0);
+    private BigDecimal num2 = new BigDecimal(0);
     private char sign = '\0';
     private String[] symbols;
 
@@ -13,42 +17,47 @@ public class Calculator {
 
     public void operation() {
         try {
-            num1 = Double.parseDouble(symbols[0]);
-            sign = symbols[1].charAt(0);
-            switch (sign) {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '^':
-                case '%':
-                    break;
-                default:
-                    System.out.println("Don't support type!");
-            }
-            num2 = Double.parseDouble(symbols[2]);
+            num1 = BigDecimal.valueOf(Double.parseDouble(symbols[0]));
         } catch (Exception e) {
-            System.out.println("Error!!!" + " Input the num1/operation/num2 into array by space!");
+            System.out.println("Error!!!Not number or not add the operation!");
+        }
+
+        sign = symbols[1].charAt(0);
+        switch (sign) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+            case '%':
+                break;
+            default:
+                System.out.println("Don't support type!");
+        }
+        try {
+            num2 = BigDecimal.valueOf(Double.parseDouble(symbols[2]));
+        } catch (Exception e) {
+            System.out.println("Error!!!Edit pls the second number,not symbol or any others variables");
         }
     }
 
-    public double calculate() {
+    public BigDecimal calculate() {
         switch (sign) {
             case '+':
-                return num1 + num2;
+                return num1.add(num2);
             case '-':
-                return num1 - num2;
+                return num1.subtract(num2);
             case '*':
-                return num1 * num2;
+                return num1.multiply(num2);
             case '/':
-                return num1 / num2;
+                return num1.divide(num2, 10, ROUND_HALF_UP);
             case '^':
-                return Math.pow(num1, num2);
-            case '%':
-                return num1 % num2;
+                return num1.pow(num2.intValue());
+            //  case '%':
+            //      return num1 % num2;
             default:
                 System.out.println("Calc Error!");
         }
-        return sign;
+        return new BigDecimal(0);
     }
 }
